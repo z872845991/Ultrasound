e540decae6e1d262e6e2ec1939488ed823a57f21
Vegas <z872845991@163.com>
2021-10-20 16:25:40 +0800

    Update train_unet_local.py

--- train/train_unet_local.py
+++ train/train_unet_local.py
@@ -80,14 +80,14 @@ class train_model_local():
                             te_avgmeter5.update(TNR1)
                             te_avgmeter6.update(TPR1)
                             te_avgmeter7.update(F11)
-                            if epoch==80:
-                                path3=path3+'_%d.pth'%epoch
-                                torch.save(self.model.state_dict(),path3)
+                            if epoch==(self.num_epochs-1):
                                 if iou1 < threshold:
                                     with open('/content/drive/MyDrive/result/hard/hard_seg_7m_unet_change.txt', 'a+') as file:
                                         for s in z:
                                             file.write(s + '\n')
-
+                    if epoch==(self.num_epochs-1):
+                        path3=path3+'_%d.pth'%epoch
+                        torch.save(self.model.state_dict(),path3)
                     with open(file2,'a+') as fileval:
                         fileval.write(" ACC:%.4f  PPV:%.4f  TNR:%.4f  TPR:%.4f  F1:%.4f  miou:%.4f maxiou:%.4f miniou:%.4f  mdice:%.4f maxdice:%.4f mindice:%.4f iou1:%.4f iou2:%.4f iou3:%.4f iou4:%.4f iou5:%.4f iou6:%.4f iou7:%.4f iou8:%.4f dice1:%.4f dice2:%.4f dice3:%.4f dice4:%.4f dice5:%.4f dice6:%.4f dice7:%.4f dice8:%.4f" % (
                             te_avgmeter3.avg, te_avgmeter4.avg, te_avgmeter5.avg, te_avgmeter6.avg, te_avgmeter7.avg, te_avgmeter1.avg, te_avgmeter1.max, te_avgmeter1.min, te_avgmeter2.avg, te_avgmeter2.max,te_avgmeter2.min, te_avgmeter1.first, te_avgmeter1.second, te_avgmeter1.third, te_avgmeter1.forth, te_avgmeter1.fifth, te_avgmeter1.sixth, te_avgmeter1.seventh, te_avgmeter1.eighth, te_avgmeter2.first, te_avgmeter2.second, te_avgmeter2.third, te_avgmeter2.forth, te_avgmeter2.fifth, te_avgmeter2.sixth, te_avgmeter2.seventh, te_avgmeter2.eighth) + '\n')
