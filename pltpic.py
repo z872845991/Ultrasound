4835f1888777e5966107d3c0e68f247774d0757f
z872845991 <z872845991@163.com>
2021-11-03 17:23:20 +0800

    Teawater

--- pltpic.py
+++ pltpic.py
@@ -5,8 +5,8 @@ from torch.utils.data import DataLoader
 from torch import nn, optim
 from torchvision.transforms import transforms
 from dataset.Fetus import FetusDataset
-from model.unet import Unet
-
+# from model.unet import Unet
+from model.unet_embedd_nonlocal import Unet_embedd_nonlocal
 x_transforms = transforms.Compose([
     transforms.Resize((512,512)),
     transforms.ToTensor()
@@ -17,11 +17,11 @@ y_transforms = transforms.Compose([
     transforms.Resize((512, 512)),
     transforms.ToTensor()
    ])
-Path='E:\\Broswer\\train_7m_Unet_change2_80.pth'
+Path='E:\\Broswer\\train_7m_Unet_embedd_nonlocal_change_80.pth'
 test_dataset = FetusDataset("F:\\test", mode='train',transform=x_transforms,target_transform=y_transforms)
 dataloaders = DataLoader(test_dataset, batch_size=1)
 with torch.no_grad():
-    model=Unet(1) 
+    model=Unet_embedd_nonlocal(1) 
 
     data=next(iter(dataloaders))  #dataloaders val
     pic,label=data[0],data[1]
