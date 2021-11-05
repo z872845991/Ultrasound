2f17cdf4f2c834dbc8779d7fcc81724b74264c79
z872845991 <z872845991@163.com>
2021-11-05 19:54:25 +0800

    autofocus

--- model/Autofocus_unet_v2.py
+++ model/Autofocus_unet_v2.py
@@ -5,48 +5,57 @@ import torch
 import numpy as np
 from torch.autograd import Variable
 import torch.nn.functional as F
-
+from torchsummary.torchsummary import summary
+def Double_conv(in_channel,out_channel):
+    return nn.Sequential(
+        nn.Conv2d(in_channel,out_channel,3,stride=1,padding=1),
+        nn.BatchNorm2d(out_channel),
+        nn.GELU(),
+        nn.Conv2d(out_channel,out_channel,kernel_size=3,stride=1,padding=1),
+        nn.BatchNorm2d(out_channel),
+        nn.GELU()
+    )
 class Autofocus_conv(nn.Module):
-    def __init__(self,input_channel,output_channel,num_branches=4):
+    def __init__(self,input_channel,output_channel,num_branches=3):
         super(Autofocus_conv,self).__init__()
         self.num_branches=num_branches
-        self.conv11=nn.Conv2d(input_channel,output_channel,kernel_size=3,dilation=2)
-        self.conv12=nn.Conv2d(input_channel,output_channel,kernel_size=3,dilation=6,padding=4)
-        self.conv13=nn.Conv2d(input_channel,output_channel,kernel_size=3,dilation=10,padding=8)
-        self.conv14=nn.Conv2d(input_channel,output_channel,kernel_size=3,dilation=14,padding=12)
-        self.convatt11=nn.Conv2d(input_channel,int(input_channel/2),kernel_size=3,dilation=2)
-        self.convatt12=nn.Conv2d(int(input_channel),out_channels=num_branches,kernel_size=1)
+        self.conv11=nn.Conv2d(input_channel,output_channel//2,kernel_size=3,dilation=2,padding=2)
+        self.conv12=nn.Conv2d(input_channel,output_channel//2,kernel_size=3,dilation=6,padding=6)
+        self.conv13=nn.Conv2d(input_channel,output_channel//2,kernel_size=3,dilation=10,padding=10)
+        #self.conv14=nn.Conv2d(input_channel,output_channel//2,kernel_size=3,dilation=14,padding=14)
+        self.convatt11=nn.Conv2d(input_channel,input_channel//2,kernel_size=3,dilation=2,padding=2)
+        self.convatt12=nn.Conv2d(input_channel//2,out_channels=num_branches,kernel_size=1)
        
-        self.conv21=nn.Conv2d(input_channel,output_channel,kernel_size=3,dilation=2)
-        self.conv22=nn.Conv2d(input_channel,output_channel,kernel_size=3,dilation=6,padding=4)
-        self.conv23=nn.Conv2d(input_channel,output_channel,kernel_size=3,dilation=10,padding=8)
-        self.conv24=nn.Conv2d(input_channel,output_channel,kernel_size=3,dilation=14,padding=12)
+        self.conv21=nn.Conv2d(output_channel//2,output_channel,kernel_size=3,dilation=2,padding=2)
+        self.conv22=nn.Conv2d(output_channel//2,output_channel,kernel_size=3,dilation=6,padding=6)
+        self.conv23=nn.Conv2d(output_channel//2,output_channel,kernel_size=3,dilation=10,padding=10)
+        #self.conv24=nn.Conv2d(output_channel//2,output_channel,kernel_size=3,dilation=14,padding=14)
 
-        self.convatt21=nn.Conv2d(output_channel,int(output_channel/2),kernel_size=3,dilation=2)
-        self.convatt22=nn.Conv2d(int(output_channel/2),num_branches,kernel_size=1)
+        self.convatt21=nn.Conv2d(output_channel//2,output_channel//2,kernel_size=3,dilation=2,padding=2)
+        self.convatt22=nn.Conv2d(output_channel//2,num_branches,kernel_size=1)
         
-        self.Bn=nn.BatchNorm2d(output_channel)
-
+        self.Bn=nn.BatchNorm2d(output_channel//2)
+        self.Bn2=nn.BatchNorm2d(output_channel)
         self.relu=nn.ReLU(inplace=True)
 
-        self.donwsample=nn.Sequential(
-            nn.Conv2d(input_channel,output_channel,kernel_size=1),
-            nn.BatchNorm2d(output_channel)
-        )
+        #self.donwsample=nn.Sequential(
+        #    nn.Conv2d(input_channel,output_channel,kernel_size=1),
+        #    nn.BatchNorm2d(output_channel)
+        #)
     def forward(self,x):
         att=self.relu(self.convatt11(x))
         att=self.convatt12(att)
         att=F.softmax(att,dim=1)
-        
+
         x11=self.conv11(x)
         x12=self.conv12(x)
         x13=self.conv13(x)
-        x14=self.conv14(x)
+        #x14=self.conv14(x)
         x11=self.Bn(x11)
         x12=self.Bn(x12)
         x13=self.Bn(x13)
-        x14=self.Bn(x14)
-        x11=x11*att[:,0:1,:,:]+x12*att[:,1:2,:,:]+x13*att[:,2:3,:,:]+x14*att[:,3:4,:,:]
+        #x14=self.Bn(x14)
+        x11=x11*att[:,0:1,:,:]+x12*att[:,1:2,:,:]+x13*att[:,2:3,:,:]
         x=self.relu(x11)
 
         # compute attention weights for the second autofocus layer
@@ -59,24 +68,24 @@ class Autofocus_conv(nn.Module):
         x21 = self.conv21(x)
         x22=self.conv22(x)
         x23=self.conv23(x)
-        x24=self.conv24(x)
-        x21=self.Bn(x21)
-        x22=self.Bn(x22)
-        x23=self.Bn(x23)
-        x24=self.Bn(x24)
-        x21=x21*att2[:,0:1,:,:]+x22*att2[:,1:2,:,:]+x23*att2[:,2:3,:,:]+x24*att2[:,3:4,:,:]
+        #x24=self.conv24(x)
+        x21=self.Bn2(x21)
+        x22=self.Bn2(x22)
+        x23=self.Bn2(x23)
+        #x24=self.Bn(x24)
+        x21=x21*att2[:,0:1,:,:]+x22*att2[:,1:2,:,:]+x23*att2[:,2:3,:,:]
         x21=self.relu(x21)
-        x = self.relu(21)
+        x = self.relu(x21)
         return x
 
 class Unet(nn.Module):
     def __init__(self,n_class):
         super().__init__()
-        self.conv_down1=Autofocus_conv(3,64)
-        self.conv_down2=Autofocus_conv(64,128)
-        self.conv_down3=Autofocus_conv(128,256)
-        self.conv_down4=Autofocus_conv(256,512)
-        self.conv_down5=Autofocus_conv(512,1024)
+        self.conv_down1=Double_conv(3,64)
+        self.conv_down2=Double_conv(64,128)
+        self.conv_down3=Double_conv(128,256)
+        self.conv_down4=Double_conv(256,512)
+        self.conv_down5=Double_conv(512,1024)
 
         self.maxpool=nn.MaxPool2d(2)
 
@@ -128,3 +137,6 @@ class Unet(nn.Module):
 
         return output
 
+if __name__=='__main__':
+    model=Unet(1)
+    summary(model,(3,224,224))
