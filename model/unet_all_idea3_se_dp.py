6cbb2cee0d31d18374aa444cc3cb2924c1f00817
z872845991 <z872845991@163.com>
2021-11-10 16:32:16 +0800

    up

--- model/unet_all_idea3_se_dp.py
+++ model/unet_all_idea3_se_dp.py
@@ -23,7 +23,7 @@ def double_conv(in_channels,out_channels):
         nn.BatchNorm2d(out_channels),
         nn.ReLU(inplace=True)
     )
-class Unet_all_idea3_se(nn.Module):
+class Unet_all_idea3_se_dp(nn.Module):
     def __init__(self,n_class,decay=2):
         super().__init__()
 
@@ -97,5 +97,5 @@ class Unet_all_idea3_se(nn.Module):
         return output
 
 if __name__=='__main__':
-    model=Unet_all_idea3_se(1,decay=4)
+    model=Unet_all_idea3_se_dp(1,decay=4)
     summary(model,(3,224,224))
